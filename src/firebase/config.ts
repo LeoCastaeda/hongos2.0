6750afcbb4599112097583cf7c0bd4602447e6fe
Leo castañeda <leocastaeda11@gmail.com>
2025-10-12 18:01:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9151698267-f2db5",
+  "appId": "1:660845217089:web:9134003650365738ac8dc0",
+  "apiKey": "AIzaSyDwIWLoxWMLLGD7YksvgFQXq80rRU8u23c",
+  "authDomain": "studio-9151698267-f2db5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "660845217089"
+};
